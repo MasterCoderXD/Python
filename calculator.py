dd2cf6b726981c760457f2c42e49640f2187bba1
JackFly26 <33792615+JackFly26@users.noreply.github.com>
2018-03-06 21:06:55 -0500

    Create calculator.py
    I suck at python, so please correct

---
+++ calculator.py
@@ -0,0 +1,23 @@
+calculate();
+def calculate():
+  d=int(0);
+  x = input("num 1? ");
+  y = input("num 2? ");
+  z =int(x);
+  za=int(y);
+  command=input("which function \n m=multiply\n d=divide \n s=subtract \n a=addition: ");
+   if command=="m":
+    d=int(z*za);
+    print(d);
+
+  elif command=="s":
+    d = z-za;
+    print(d);
+
+  elif command=="a":
+    d=z+za;
+    print(d);
+
+  elif command =="d":
+    d=z/za;
+    print(d);
