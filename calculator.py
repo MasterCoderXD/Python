7406ec2296d2797279c85d12c7804894fb835197
JackFly26 <33792615+JackFly26@users.noreply.github.com>
2018-03-06 21:26:16 -0500

    added exponents and formatting

--- calculator.py
+++ calculator.py
@@ -1,23 +1,28 @@
-calculate();
 def calculate():
-  d=int(0);
-  x = input("num 1? ");
-  y = input("num 2? ");
-  z =int(x);
-  za=int(y);
-  command=input("which function \n m=multiply\n d=divide \n s=subtract \n a=addition: ");
-  if command=="m":
-    d=int(z*za);
-    print(d);
+    d = int(0)
+    x = input("num 1? ")
+    y = input("num 2? ")
+    z = int(x)
+    za = int(y)
+    command = input(
+        "which function \nm=multiply\n d=divide \n s=subtract \n a=addition \n e=exponent \n"
+    )
+    if command == "m":
+        d = int(z * za)
+        print(d)
 
-  elif command=="s":
-    d = z-za;
-    print(d);
+    elif command == "s":
+        d = z - za
+        print(d)
 
-  elif command=="a":
-    d=z+za;
-    print(d);
+    elif command == "a":
+        d = z + za
+        print(d)
 
-  elif command =="d":
-    d=z/za;
-    print(d);
+    elif command == "d":
+        d = z / za
+        print(d)
+
+    elif command == "e":
+        d = z**za
+        print(d)
